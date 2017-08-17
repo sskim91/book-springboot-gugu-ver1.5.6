10de3df781863d721dca73177ae41215c38b87e6
sskim <tjdtjq91@gmail.com>
2017-08-17 16:41:17 +0900

    Spring Data JPA 이용한 단순 게시물의 처리 쿼리 메소드 이용 및 테스트 코드 Pageable 인터페이스/ 페이징 처리와 정렬 @Query를 이용

--- src/main/java/com/sskim/persistence/BoardRepository.java
+++ src/main/java/com/sskim/persistence/BoardRepository.java
@@ -1,7 +1,65 @@
 package com.sskim.persistence;
 
 import com.sskim.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
+
+    //제목 찾기
+    public List<Board> findBoardByTitle(String title);
+
+    //작성자 찾기
+    public Collection<Board> findByWriter(String writer);
+
+    //작성자에 대한 like % 키워드 %
+    public Collection<Board> findByWriterContaining(String writer);
+
+    //OR 조건의 처리
+    public Collection<Board> findByTitleContainingOrContentContaining(String title, String content);
+
+    //title Like % ? % and bno > ?
+    public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword, Long num);
+
+    //bno > ? ORDER BY bno DESC
+    public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+
+    //bno > ? ORDER BY bno DESC limit ?, ?
+    public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+
+    //public List<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+    //인터페이스에 페이징, 정렬 처리
+    public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+    //@Query 작성 - 제목에 대한 검색 처리
+    @Query("SELECT b FROM  Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    public List<Board> findByTitle(String title);
+
+    //내용에 대한 검색 처리 @Param
+    @Query("SELECT b FROM  Board b WHERE b.title LIKE %:content% AND b.bno > 0 ORDER BY b.bno DESC")
+    public List<Board> findByContent(@Param("content") String content);
+
+    //작성자에 대한 검색 처리 - #{entityName}
+    //@Query("SELECT b FROM #{entityName} b WHERE b.writer LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    //public List<Board> findByWriter2(String writer);
+
+    @Query("SELECT b.bno, b.title, b.writer, b.regdate " + " FROM Board b WHERE b.title LIKE %?1% AND b.bno > 0 ORDER BY b.bno DESC")
+    public List<Object[]> findByTitle2(String title);
+
+    //nativeQuery 사용
+    @Query(value = "SELECT bno, title, writer FROM tbl_boards WHERE title " +
+            "LIKE CONCAT('%' ?1, '%') AND bno > 0 ORDER BY bno DESC", nativeQuery = true)
+    public List<Object[]> findByTitle3(String title);
+
+    //@Query와 Pagigng 처리/정렬
+    @Query("SELECT b FROM Board b WHERE b.bno > 0 ORDER BY b.bno DESC")
+    public List<Board> findBypage(Pageable pageable);
+
 }
--- src/test/java/com/sskim/BoardRepositoryTests.java
+++ src/test/java/com/sskim/BoardRepositoryTests.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
@@ -72,4 +79,119 @@ public class BoardRepositoryTests {
         System.out.println("DELETE Entity..");
         boardRepository.delete(1L);
     }
+
+    @Test
+    public void 데이터_넣기() {
+
+        for (int i = 1; i <= 200; i++) {
+
+            Board board = new Board();
+            board.setTitle("제목..." + i);
+            board.setContent("내용..." + i + " 채우기");
+            board.setWriter("user0" + (i % 10));
+            boardRepository.save(board);
+        }
+    }
+
+    @Test
+    public void 제목으로_게시물찾기() {
+//    before Java8
+//        List<Board> list = boardRepository.findBoardByTitle("제목...177");
+//
+//        for(int i=0, len=list.size(); i<len; i++) {
+//            System.out.println(list.get(i));
+//        }
+
+        //Java8
+        boardRepository.findBoardByTitle("제목...177")
+                .forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 작성자로_검색하기() {
+        Collection<Board> results = boardRepository.findByWriter("user00");
+
+        results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 제목_검색_05가포함() {
+        Collection<Board> results = boardRepository.findByWriterContaining("05");
+        //for 루프 대신 forEach
+        results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 제목과_부등호검색() {
+
+        Collection<Board> results = boardRepository.findByTitleContainingAndBnoGreaterThan("5", 50L);
+
+        results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 정렬테스트() {
+
+        Collection<Board> results = boardRepository.findByBnoGreaterThanOrderByBnoDesc(90L);
+
+        results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 페이징_테스트() {
+
+        Pageable paging = new PageRequest(0, 10);
+
+        Collection<Board> results = boardRepository.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+
+        results.forEach(board -> System.out.println(board));
+    }
+
+    //    @Test
+//    public void 페이지처리와_정렬테스트() {
+//
+//        Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+//
+//        Collection<Board> results = boardRepository.findByBnoGreaterThan(0L, paging);
+//
+//        results.forEach(board -> System.out.println(board));
+//    }
+    @Test
+    public void 페이지처리와_정렬테스트_인터페이스() {
+
+        Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+
+        Page<Board> result = boardRepository.findByBnoGreaterThan(0L, paging);
+
+        System.out.println("PAGE SIZE : " + result.getSize());
+        System.out.println("TOTAL PAGE : " + result.getTotalPages());
+        System.out.println("TOTAL COUNT : " + result.getTotalElements());
+        System.out.println("NEXT : " + result.nextPageable());
+
+        List<Board> list = result.getContent();
+
+        list.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void JPQL_테스트() {
+        boardRepository.findByTitle("17")
+                .forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void 필요한칼럼만_추출하는경우() {
+        boardRepository.findByTitle2("17")
+                .forEach(arr -> System.out.println(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void 쿼리와_페이지정렬() {
+
+        Pageable pageable = new PageRequest(0, 10);
+
+        boardRepository.findBypage(pageable)
+                .forEach(board -> System.out.println(board));
+    }
+
 }
